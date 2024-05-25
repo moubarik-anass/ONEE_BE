21777a94ecf67acfbcdbd4a258b1d2f81657a834
moubarik-anass <111663633+moubarik-anass@users.noreply.github.com>
2024-05-25 14:49:13 +0100

    popup offre

--- ONEE_BE_v2/Controllers/OffresController.cs
+++ ONEE_BE_v2/Controllers/OffresController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ONEE_BE_v2.Context;
 using ONEE_BE_v2.Models;
@@ -24,32 +19,36 @@ namespace ONEE_BE_v2.Controllers
         {
             return View();
         }
+
         [HttpGet]
         public JsonResult GetOffres()
         {
             var offres = _context.Offres.ToList();
             return Json(offres);
         }
+
         [HttpPost]
         public JsonResult Insert(Offre model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _context.Offres.Add(model);
                 _context.SaveChanges();
-                return Json("les details d offres sont enregistrées");
+                return Json("Les détails de l'offre sont enregistrés");
             }
             else
             {
                 return Json("Problème de validation");
             }
         }
+
         [HttpGet]
         public JsonResult Edit(int id)
         {
-            var offres = _context.Offres.Find(id);
-            return Json(offres);
+            var offre = _context.Offres.Find(id);
+            return Json(offre);
         }
+
         [HttpPost]
         public JsonResult Update(Offre model)
         {
@@ -57,22 +56,23 @@ namespace ONEE_BE_v2.Controllers
             {
                 _context.Offres.Update(model);
                 _context.SaveChanges();
-                return Json("les details d offres sont modifées");
+                return Json("Les détails de l'offre sont modifiés");
             }
             else
             {
                 return Json("Problème de validation");
             }
         }
+
         [HttpPost]
         public JsonResult Delete(int id)
         {
-            var offres = _context.Offres.Find(id);
-            if (offres != null)
+            var offre = _context.Offres.Find(id);
+            if (offre != null)
             {
-                _context.Offres.Remove(offres);
+                _context.Offres.Remove(offre);
                 _context.SaveChanges();
-                return Json("les details d offres sont supprimées");
+                return Json("Les détails de l'offre sont supprimés");
             }
             else
             {
--- ONEE_BE_v2/wwwroot/js/Offre.js
+++ ONEE_BE_v2/wwwroot/js/Offre.js
@@ -5,78 +5,79 @@ $(document).ready(function () {
 function GetOffres() {
     $.ajax({
         url: '/offre/GetOffres',
-        type: 'get',
+        type: 'GET',
         dataType: 'json',
         contentType: 'application/json;charset=utf-8',
         success: function (response) {
             if (response == null || response == undefined || response.length == 0) {
                 var object = '';
                 object += '<tr>';
-                object += '<td colspan="5">' + 'Les offres ne sont pas disponnibles' + '</td>';
+                object += '<td colspan="7">Aucune offre disponible</td>';
                 object += '</tr>';
                 $('#tblBody').html(object);
-            }
-            else {
+            } else {
                 var object = '';
                 $.each(response, function (index, item) {
                     object += '<tr>';
-                    object += '<td>' + item.Id + '</td>';
-                    object += '<td>' + item.Titre + '</td>';
+                    object += '<td>' + item.id + '</td>';
+                    object += '<td>' + item.titre + '</td>';
                     object += '<td>' + item.dateDebut + '</td>';
                     object += '<td>' + item.dateFin + '</td>';
                     object += '<td>' + item.nbr_places + '</td>';
-                    object += '<td>' + item.Description + '</td>';
-                    object += '<td> <a href="#" class="btn btn-primary btn-sm" onclick="Edit(' + item.Id + ')">Edit</a> <a href="#" class="btn btn-danger btn-sm" onclick="Delete(' + item.Id + ')">Delete</a></td>';
+                    object += '<td>' + item.description + '</td>';
+                    object += '<td><a href="#" class="btn btn-primary btn-sm" onclick="Edit(' + item.id + ')">Modifier</a> <a href="#" class="btn btn-danger btn-sm" onclick="Delete(' + item.id + ')">Supprimer</a></td>';
                     object += '</tr>';
                 });
                 $('#tblBody').html(object);
             }
         },
         error: function () {
-            alert('impossible d afficher les offres');
+            alert('Impossible d\'afficher les offres');
         }
     });
 }
 
 $('#btnAdd').click(function () {
     $('#OffreModal').modal('show');
-    $('#modalTitle').text('Ajouter Offre');
+    $('#modalTitle').text('Ajouter une offre');
+    $('#Save').show();
+    $('#Update').hide();
+    ClearData();
 });
+
 function Insert() {
     var result = Validate();
     if (result == false) {
         return false;
     }
 
-    var formData = new Object();
-    formData.Id = $('#Id').val();
-    formData.Titre = $('#Titre').val();
-    formData.dateDebut = $('#dateDebut').val();
-    formData.dateFin = $('#dateFin').val();
-    formData.nbr_places = $('#nbr_places').val();
-    formData.Description = $('#Description').val();
+    var formData = {
+        id: $('#Id').val(),
+        titre: $('#Titre').val(),
+        dateDebut: $('#dateDebut').val(),
+        dateFin: $('#dateFin').val(),
+        nbr_places: $('#nbr_places').val(),
+        description: $('#Description').val()
+    };
 
     $.ajax({
         url: '/offre/Insert',
+        type: 'POST',
         data: formData,
-        type: 'post',
         success: function (response) {
             if (response == null || response == undefined || response.length == 0) {
-                alert('impossible de enregistrer les données');
-            }
-            else {
+                alert('Impossible d\'enregistrer les données');
+            } else {
                 HideModal();
                 GetOffres();
                 alert(response);
             }
         },
         error: function () {
-            alert('impossible d afficher les offres');
+            alert('Impossible d\'enregistrer les données');
         }
     });
-
 }
-
 
 function HideModal() {
     ClearData();
@@ -84,48 +85,47 @@ function HideModal() {
 }
 
 function ClearData() {
-    //$('#Id').val();
-    $('#Titre').val();
-    $('#dateDebut').val();
-    $('#dateFin').val();
-    $('#nbr_places').val();
-    $('#Description').val();
+    $('#Id').val('');
+    $('#Titre').val('');
+    $('#dateDebut').val('');
+    $('#dateFin').val('');
+    $('#nbr_places').val('');
+    $('#Description').val('');
 
     $('#Titre').css('border-color', 'lightgrey');
     $('#dateDebut').css('border-color', 'lightgrey');
     $('#dateFin').css('border-color', 'lightgrey');
     $('#nbr_places').css('border-color', 'lightgrey');
     $('#Description').css('border-color', 'lightgrey');
-
 }
 
 function Validate() {
     var isValid = true;
-    if ($('#Titre').val().trim() == "") {
+    if ($('#Titre').val().trim() === "") {
         $('#Titre').css('border-color', 'Red');
         isValid = false;
     } else {
         $('#Titre').css('border-color', 'lightgrey');
     }
-    if ($('#dateDebut').val().trim() == "") {
+    if ($('#dateDebut').val().trim() === "") {
         $('#dateDebut').css('border-color', 'Red');
         isValid = false;
     } else {
         $('#dateDebut').css('border-color', 'lightgrey');
     }
-    if ($('#dateFin').val().trim() == "") {
+    if ($('#dateFin').val().trim() === "") {
         $('#dateFin').css('border-color', 'Red');
         isValid = false;
     } else {
         $('#dateFin').css('border-color', 'lightgrey');
     }
-    if ($('#nbr_places').val().trim() == "") {
+    if ($('#nbr_places').val().trim() === "") {
         $('#nbr_places').css('border-color', 'Red');
         isValid = false;
     } else {
         $('#nbr_places').css('border-color', 'lightgrey');
     }
-    if ($('#Description').val().trim() == "") {
+    if ($('#Description').val().trim() === "") {
         $('#Description').css('border-color', 'Red');
         isValid = false;
     } else {
@@ -133,105 +133,102 @@ function Validate() {
     }
     return isValid;
 }
+
 $('#Titre').change(function () {
     Validate();
-})
+});
 $('#dateDebut').change(function () {
     Validate();
-})
+});
 $('#dateFin').change(function () {
     Validate();
-})
+});
 $('#nbr_places').change(function () {
     Validate();
-})
+});
 $('#Description').change(function () {
     Validate();
-})
-
+});
 
 function Edit(id) {
     $.ajax({
-        url: '/offre/Edit?id' + id,
-        type: 'get',
+        url: '/offre/Edit?id=' + id,
+        type: 'GET',
         dataType: 'json',
         contentType: 'application/json;charset=utf-8',
         success: function (response) {
             if (response == null || response == undefined) {
-                alert('impossible de modifier cette offre');
-            }
-            else if (response.length == 0) {
-                alert('les données ne sont pas accessibles avec id ' + id);
-
+                alert('Impossible de modifier cette offre');
+            } else if (response.length === 0) {
+                alert('Les données ne sont pas accessibles avec l\'ID ' + id);
             } else {
                 $('#OffreModal').modal('show');
-                $('#modalTitle').text('Modifier Offre');
-                $('#Save').css('display', 'none');
-                $('#Update').css('display', 'block');
-                $('#Id').val(response.Id);
-                $('#Titre').val(response.Titre);
+                $('#modalTitle').text('Modifier une offre');
+                $('#Save').hide();
+                $('#Update').show();
+                $('#Id').val(response.id);
+                $('#Titre').val(response.titre);
                 $('#dateDebut').val(response.dateDebut);
                 $('#dateFin').val(response.dateFin);
                 $('#nbr_places').val(response.nbr_places);
-                $('#Description').val(response.Description);
+                $('#Description').val(response.description);
             }
         },
         error: function () {
-            alert('impossible de modifier les données');
+            alert('Impossible de modifier les données');
         }
     });
 }
 
 function Update() {
     var result = Validate();
-    if (result == false) {
+    if (result === false) {
         return false;
     }
-    var formData = new Object();
-    formData.Id = $('#Id').val();
-    formData.Titre = $('#Titre').val();
-    formData.dateDebut = $('#dateDebut').val();
-    formData.dateFin = $('#dateFin').val();
-    formData.nbr_places = $('#nbr_places').val();
-    formData.Description = $('#Description').val();
+
+    var formData = {
+        id: $('#Id').val(),
+        titre: $('#Titre').val(),
+        dateDebut: $('#dateDebut').val(),
+        dateFin: $('#dateFin').val(),
+        nbr_places: $('#nbr_places').val(),
+        description: $('#Description').val()
+    };
 
     $.ajax({
         url: '/offre/Update',
-        data: formData, 
-        type: 'post',
+        type: 'POST',
+        data: formData,
         success: function (response) {
-            if (response == null || response == undefined || response.length == 0) {
-                alert('impossible de modifier les données');
-            }
-            else {
+            if (response == null || response == undefined || response.length === 0) {
+                alert('Impossible de modifier les données');
+            } else {
                 HideModal();
                 GetOffres();
                 alert(response);
             }
         },
         error: function () {
-            alert('impossible de modifier les données');
+            alert('Impossible de modifier les données');
         }
     });
 }
 
 function Delete(id) {
-    if (confirm('Vous etes d accord de supprimer cette offre ?') {
-    $.ajax({
-        url: '/offre/Delete?id' + id,
-        type: 'post',
-        success: function (response) {
-            if (response == null || response == undefined) {
-                alert('impossible de supprimer cette offre');
-            }else {
-                GetOffres();
-                alert('impossible d afficher les offres');
+    if (confirm('Vous êtes d\'accord pour supprimer cette offre ?')) {
+        $.ajax({
+            url: '/offre/Delete?id=' + id,
+            type: 'POST',
+            success: function (response) {
+                if (response == null || response == undefined) {
+                    alert('Impossible de supprimer cette offre');
+                } else {
+                    GetOffres();
+                }
+            },
+            error: function () {
+                alert('Impossible de supprimer cette offre');
             }
-        },
-        error: function () {
-            alert('impossible de supprimer cette offre');
-        }
-    });
-
+        });
     }
 }
